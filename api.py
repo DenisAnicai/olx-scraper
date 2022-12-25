fb5e964166d2e760e855a46f33ba7afafaa05e4f
Denis Anicai <denisanicai14@gmail.com>
2022-12-26 01:19:09 +0200

    fix

--- api.py
+++ api.py
@@ -29,9 +29,7 @@ if __name__ == '__main__':
 
     uvicorn.run(
         'api:app',
-        host='127.0.0.1',
+        host='0.0.0.0',
         port=8000,
-        reload=True,
-        debug=True,
         workers=1,
     )
